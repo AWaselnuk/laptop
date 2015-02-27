f9177fe1231152108c8b52c77020dfa853047037
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2015-02-27 17:29:44 -0500

    Added custom script in same directory

---
+++ custom.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+brew_tap 'caskroom/cask'
+brew_install_or_upgrade 'brew-cask'
+
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install rdio
+
+gem_install_or_update 'parity'
+
+brew_install_or_upgrade 'tree'
+brew_install_or_upgrade 'watch'
