b552a228004b0b92d56385120f76186005c090a6
Adam Waselnuk <awaselnuk@mygrocerydeals.com>
2015-03-06 12:47:35 -0500

    Added my cask apps.

--- custom.sh
+++ custom.sh
@@ -3,9 +3,35 @@
 brew_tap 'caskroom/cask'
 brew_install_or_upgrade 'brew-cask'
 
-brew cask install dropbox
+# Dev Tools
+brew cask install sublime-text
+brew cask install adobe-creative-cloud
+
+# Google Apps
+brew cask install google-drive
+brew cask install google-hangouts
 brew cask install google-chrome
-brew cask install rdio
+
+# Misc. Apps
+brew cask install dropbox
+brew cask install firefox
+brew cask install spotify
+brew cask install evernote
+brew cask install steam
+brew cask install shiftit
+brew cask install skype
+brew cask install sketch
+brew cask install the-unarchiver
+brew cask install time-out
+brew cask install vlc
+
+# Mac Office - None of these casks exist
+# brew cask install pages
+# brew cask install keynote
+
+# Misc. Non-Existant Casks
+# brew cask install sip
+# brew cask install wunderlist
 
 gem_install_or_update 'parity'
 
